bc23741a7dc96a9652a7f0776749cbc473b14174
Jordan Gardner <jordanthomasg@gmail.com>
2025-04-02 17:21:28 -0600

    Add page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Head from "next/head";
 import QRCode from "qrcode";
 import { useState } from "react";
 
@@ -34,52 +35,57 @@ export default function Home() {
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full">
-        {dataUrl ? (
-          <div className="flex flex-col justify-center items-center gap-4 w-full">
-            <img id="qrcode" src={dataUrl} className="size-40" />
-            <div className="flex justify-center items-center gap-4">
-              <button
-                type="button"
-                className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
-                onClick={handleDownload}
-              >
-                Download
-              </button>
-              <button
-                type="button"
-                className="rounded-md bg-indigo-50 px-2.5 py-1.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100 cursor-pointer"
-                onClick={handleReset}
-              >
-                Reset
-              </button>
-            </div>
-          </div>
-        ) : (
-          <form onSubmit={handleSubmit} className="w-full">
+    <>
+      <Head>
+        <title>QR Code Generator</title>
+      </Head>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full">
+          {dataUrl ? (
             <div className="flex flex-col justify-center items-center gap-4 w-full">
-              <h1 className="italic">For Austin</h1>
-              <input
-                type="text"
-                placeholder="https://example.com"
-                className="w-full border border-gray-900 dark:border-gray-300 rounded-sm p-2 w-full max-w-[400px] focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                value={url}
-                onChange={(e) => setUrl(e.target.value)}
-              />
-              <div className="flex justify-center items-center mb-4">
+              <img id="qrcode" src={dataUrl} className="size-40" />
+              <div className="flex justify-center items-center gap-4">
                 <button
-                  type="submit"
+                  type="button"
                   className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
-                  disabled={!url}
+                  onClick={handleDownload}
                 >
-                  Generate QR Code
+                  Download
+                </button>
+                <button
+                  type="button"
+                  className="rounded-md bg-indigo-50 px-2.5 py-1.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100 cursor-pointer"
+                  onClick={handleReset}
+                >
+                  Reset
                 </button>
               </div>
             </div>
-          </form>
-        )}
-      </main>
-    </div>
+          ) : (
+            <form onSubmit={handleSubmit} className="w-full">
+              <div className="flex flex-col justify-center items-center gap-4 w-full">
+                <h1 className="italic">For Austin</h1>
+                <input
+                  type="text"
+                  placeholder="https://example.com"
+                  className="w-full border border-gray-900 dark:border-gray-300 rounded-sm p-2 w-full max-w-[400px] focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                  value={url}
+                  onChange={(e) => setUrl(e.target.value)}
+                />
+                <div className="flex justify-center items-center mb-4">
+                  <button
+                    type="submit"
+                    className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
+                    disabled={!url}
+                  >
+                    Generate QR Code
+                  </button>
+                </div>
+              </div>
+            </form>
+          )}
+        </main>
+      </div>
+    </>
   );
 }
